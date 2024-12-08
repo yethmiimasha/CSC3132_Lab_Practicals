ae6eaa206700c9d2d4c66c2b10545ae82560fdd0
yethmiimasha <139648583+yethmiimasha@users.noreply.github.com>
2024-12-08 12:44:53 +0000

    index file created

---
+++ 06.12.2024/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>2020/ASP/02</h1><br><br>
+
+    <a href="getdata.php">Gettable</a><br>
+    
+    <a href="print.php">Print</a><br>
+
+
+    
+</body>
+</html>
