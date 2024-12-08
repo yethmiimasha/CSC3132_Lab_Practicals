d90e7932022c4724fcb24d48d9c045b2fd4b4d40
yethmiimasha <139648583+yethmiimasha@users.noreply.github.com>
2024-12-08 12:43:45 +0000

    getdata file created

---
+++ 06.12.2024/getdata.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>EMPLOYEE LIST</title>
+</head>
+<body>
+
+	<?php
+//get the db connection file
+
+require_once 'dbconf.php'; //(conf/dbconf.php) (folder/file)
+require_once 'myfunc.php';
+
+GetData($connect);
+
+?>
+
+
+
+</body>
+</html>
