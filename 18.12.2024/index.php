90a0cbe4e82e2714486482022137299e24e58579
yethmiimasha <139648583+yethmiimasha@users.noreply.github.com>
2024-12-26 10:57:31 +0000

    index file created

---
+++ 18.12.2024/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Practicals</h1><br><br>
+
+    <a href="gettable.php">Gettable</a><br>
+    <a href="printtable.php">printtable</a><br>
+
+    
+</body>
+</html>
