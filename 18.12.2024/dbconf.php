415491912b416389c38cd7f8a338f2b7f3b421eb
yethmiimasha <139648583+yethmiimasha@users.noreply.github.com>
2024-12-26 10:54:54 +0000

    dbconf file created

---
+++ 18.12.2024/dbconf.php
@@ -0,0 +1,24 @@
+<?php
+//constant variable (variable name, value)
+/*define('SERVERNAME', '127.0.0.1:3306');
+define('USERNAME', 'root');
+define('PASSWORD', 'mariadb');*/
+define('SERVERNAME','localhost');
+define('USERNAME','root');
+define('PASSWORD','');
+define('DBNAME', 'library');
+try{
+	
+$connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
+
+if (!$connect) {
+	die("connection failed".mysqli_connect_error()); //die - stop process after that
+} else {
+	//echo "Connection successfully <br>";
+	}
+}
+catch (Exception $e){
+	die($e->getMessage());
+}
+
+?>
